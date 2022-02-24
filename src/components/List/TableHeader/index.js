2b81492f200ffa9a3580881bf9dde6e093ce86e9
Felipe Carvalho Nonato Matos <felipe.matos@XDNTRAVEL.CORP>
2022-02-24 15:06:24 -0300

    feat: integration table header

--- src/components/List/TableHeader/index.js
+++ src/components/List/TableHeader/index.js
@@ -1,13 +1,64 @@
 import './style.css'
+import ordenar from '../../../assets/ordenar.svg';
+import desordenar from '../../../assets/desordenar.svg';
+import { useState } from 'react';
 
 function TableHeader(){
+    const [filter, setFilter] = useState('date');
+    const [order, setOrder] = useState('asc');
+
+    function handleChangeFilter(type){
+        if (filter === type) {
+            setOrder(order === 'asc' ? 'desc' : 'asc')
+            return;
+        }
+        setFilter(type);
+    }
+
     return (
         <div className='table-head'>
-            <div className='column-title'>Data</div>
-            <div className='column-title'>Dia da Semana</div>
-            <div className='column-title'>Descrição</div>
-            <div className='column-title'>Categoria</div>
-            <div className='column-title'>Valor</div>
+            <div
+            className='column-title cursor-pointer'
+            onClick={() => handleChangeFilter('date')}
+            >
+                <span>Data</span>
+                {filter === 'date' &&
+                <img 
+                src={order === 'asc' ? desordenar : ordenar} 
+                alt='ordenar'
+                />
+                }
+            </div>
+            <div 
+            className='column-title cursor-pointer'
+            onClick={() => handleChangeFilter('weekDay')}
+            >
+                <span>Dia da Semana</span>
+                {filter === 'weekDay' &&
+                <img 
+                src={order === 'asc' ? desordenar : ordenar} 
+                alt='ordenar'
+                />
+                }
+            </div>
+            <div className='column-title'>
+                <span>Descrição</span>
+            </div>
+            <div className='column-title'>
+                <span>Categoria</span>
+            </div>
+            <div 
+            className='column-title cursor-pointer'
+            onClick={() => handleChangeFilter('value')}
+            >
+                <span>Valor</span>
+                {filter === 'value' &&
+                <img 
+                src={order === 'asc' ? desordenar : ordenar} 
+                alt='ordenar'
+                />
+                }
+            </div>
         </div>
     )
 };
