50b295abd6a6df5d46fa301cb6ff1b052c018205
Felipe Carvalho Nonato Matos <felipe.matos@XDNTRAVEL.CORP>
2022-02-24 16:10:57 -0300

    feat: adding table structure, rows and row elements

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,9 +1,27 @@
-import './style.css'
+import './style.css';
 import TableHeader from './TableHeader';
+import edit from '../../assets/Edit.svg';
+import deleteicon from '../../assets/Delete.svg';
 
 function List(){
     return (
-        <TableHeader />
+        <div className='table'>
+            <TableHeader />
+            <div className='table-body'>
+                <div className='table-line'>
+                    <div className='line-items'>01/09/21</div>
+                    <div className='line-items'>Quarta</div>
+                    <div className='line-items'>Venda dos brigadeiros</div>
+                    <div className='line-items'>Pix</div>
+                    <div className='line-items'>R$ 100,00</div>
+                    <div className='line-items'>
+                        <img className='cursor-pointer' src={edit} alt='edit icon'/>
+                        <img className='cursor-pointer' src={deleteicon} alt='delete icon'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     )
 };
 
